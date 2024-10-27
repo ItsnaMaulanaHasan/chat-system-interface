4d1cda53f8c9a9d232270ce4cd35955b47b28d6d
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2024-10-27 22:34:00 +0700

    fix-bug

--- src/app/(main)/(conversations)/components/Form.tsx
+++ src/app/(main)/(conversations)/components/Form.tsx
@@ -35,7 +35,7 @@ const Form = () => {
         <MdAttachFile size={30} className="text-sky-500" />
       </CldUploadButton>
       <form onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-2 lg:gap-4 w-full">
-        <MessageInput id="message" register={register} errors={errors} required placeholder="Write a message" />
+        <MessageInput id="message" register={register} required placeholder="Write a message" />
         <button type="submit" className="rounded-full p-2 bg-sky-500 cursor-pointer hover:bg-sky-600 transition">
           <HiPaperAirplane size={18} className="text-white" />
         </button>
