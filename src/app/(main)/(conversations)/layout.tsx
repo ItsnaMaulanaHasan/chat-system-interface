99ef3c08f1d92e8051675a0980d80f17074461af
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2024-10-28 00:30:36 +0700

    fix-bug

--- src/app/(main)/(conversations)/layout.tsx
+++ src/app/(main)/(conversations)/layout.tsx
@@ -1,9 +1,12 @@
 import Sidebar from "@/components/sidebar/Sidebar";
+import React from "react";
 
 export default function ConversationsLayout({ children }: { children: React.ReactNode }) {
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <React.Suspense>{children}</React.Suspense>
+      </div>
     </Sidebar>
   );
 }
